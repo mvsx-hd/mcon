4d823b216065a6db3e6bd131fa2c2d9e6d4ae19a
MUSA HASSAN DALHAT <musahassan583@yahoo.com>
2025-11-25 12:42:51 +0100

    updates

--- app/page.jsx
+++ app/page.jsx
@@ -20,8 +20,8 @@ export default function Home() {
           <p className="text-white italic text-xl mb-8 max-w-lg">
             A community where we help each other stay healthy. Learn when to take medicines, explore practical home remedies, and read helpful insights shared by real users and medical professionals.
           </p>
-          <div className="flex items-center justify center">
-            <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="flex max-md:grid max-md:grid-cols-1 space-y-4 items-center justify center">
+            <div className="flex gap-4">
               <div className="space-x-10">
                 <button className="flex items-center justify-center gap-2 w-full bg-green-500/40 text-white px-6 py-3 rounded-lg">
                   <Link href={"/auth/signin"}>
